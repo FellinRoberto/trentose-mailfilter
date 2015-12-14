f0295e63b5cdec47bf17fb788bff04e13f99bf25
Roberto <fellin.roberto@hotmail.it>
2015-12-14 18:23:20 +0100

    fine

--- js/data.js
+++ js/data.js
@@ -1,3 +1,2 @@
-var rules = ["spam.com","compratutto.it","special offer"];
-
-var msgs = ["news@spam.com","carlo@gmail.com","jessy@compratutto.it","trentose2@googlegroups.com","maria@gmail.com"];
+var rules = ["spam.com", "compratutto.it"];
+var msgs = ["news@spam.com", "carlo@gmail.com", "jessy@compratutto.it", "trentose2@googlegroups.com"];

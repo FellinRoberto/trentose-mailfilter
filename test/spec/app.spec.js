5a1214ee4953f1efae74bc98031aa9f6919f6a0c
Roberto <fellin.roberto@hotmail.it>
2015-12-14 18:08:03 +0100

    finePrimaConsegna

--- test/spec/app.spec.js
+++ test/spec/app.spec.js
@@ -1,5 +1,17 @@
 describe("MailModel", function() {
-  it("should..", function(){
-  });
- 
+    it("testo lo stato true", function(){
+
+	MailModel.init();
+expect(MailModel.filter()).toEqual(["carlo@gmail.com", "trentose2@googlegroups.com"]);
+
+
+	
+    });
+
+    it("testo lo stato false", function(){
+	MailModel.init();
+expect(MailModel.filter()).not.toEqual(["carlo@gmail.com", "trentose2@googlegroups.com", "trentose2@googlegroups.com"]);
+	
+    });
+    
 });
